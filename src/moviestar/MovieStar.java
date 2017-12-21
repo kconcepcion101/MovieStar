d97cfa75ff0a9c98a794fdc1284949c111fc2c16
Kevin Concepcion <kconcepcion18076@columbushs.com>
2017-12-21 12:49:47 -0500

    my first commit

---
+++ src/moviestar/MovieStar.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project 
+Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moviestar;
+
+/**Kevin Concepcion
+ *Period 3
+ * Project name = MovieStarInfo
+ * @author 18076
+ */
+public class MovieStar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String actorName = "Leonardo DiCaprio";
+        char actorGender = 'M';
+        int actorAge = 42;
+        String actorBday = "November 11, 1974";
+        String actorBestAppearance = "The Revenant";
+        
+        String actressName = "Jennifer Lawrence";
+        char actressGender = 'F';
+        int actressAge = 27;
+        String actressBday = "August 15, 1990";
+        String actressBestAppearance = "The Hunger Games";
+        
+        System.out.println("Favorite Actor: " + actorName);
+        System.out.println("Gender: " + actorGender);
+        System.out.println("Age: " + actorAge);
+        System.out.println("Birthday: " + actorBday);
+        System.out.println("Best Appearance: " + actorBestAppearance);
+        
+        System.out.println("Favorite Actress: " + actressName);
+        System.out.println("Gender: " + actressGender);
+        System.out.println("Age: " + actressAge);
+        System.out.println("Birthday: " + actressBday);
+        System.out.println("Best Appearance: " + actressBestAppearance);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
